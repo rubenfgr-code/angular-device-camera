303ad758666206840a3e3a9aef16fd53ae6a7417
rubenfgr <rubenfgr87@outlook.com>
2021-07-15 12:47:50 +0200

    first version stable to testing with devices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,82 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+} from '@angular/core';
+import { createWorker } from 'tesseract.js';
 
+// declare const Buffer: typeof Buffer;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  // Prefer camera resolution nearest to 1280x720.
-  constraints = { audio: true, video: { width: 1280, height: 720 } };
+export class AppComponent implements AfterViewInit {
+  timerId!: NodeJS.Timeout;
 
-  constructor() {
+  @ViewChild('video') video!: ElementRef;
+  @ViewChild('progress') progress!: ElementRef;
+
+  ocrResult = 'Recognizing...';
+
+  ngAfterViewInit() {
     navigator.mediaDevices
-      .getUserMedia(this.constraints)
-      .then(function (mediaStream) {
-        const video = document.querySelector('video');
-        if (video) {
-          video.srcObject = mediaStream;
-          video.onloadedmetadata = function (e) {
-            video.play();
-          };
-        }
+      .getUserMedia({ video: true })
+      .then((mediaStream) => {
+        this.video.nativeElement.srcObject = mediaStream;
+        this.video.nativeElement.onloadedmetadata = () =>
+          this.video.nativeElement.play();
       })
-      .catch(function (err) {
-        console.log(err.name + ': ' + err.message);
-      }); // always check for errors at the end.
+      .catch((err) => console.log('Error:', err));
+  }
+
+  takePhoto = async () => {
+    console.log(this.video.nativeElement);
+    const canvas = document.createElement('canvas');
+    canvas.height = this.video.nativeElement.videoHeight;
+    canvas.width = this.video.nativeElement.videoWidth;
+    canvas
+      .getContext('2d')
+      ?.drawImage(
+        this.video.nativeElement,
+        0,
+        0,
+        this.video.nativeElement.videoWidth,
+        this.video.nativeElement.videoHeight
+      );
+
+    const worker = createWorker({
+      logger: (m) => {
+        const value = m.progress.toFixed(2);
+        this.progress.nativeElement.style.width = `${value * 100}%`;
+      },
+    });
+
+    await worker.load();
+    await worker.loadLanguage('spa');
+    await worker.initialize('spa');
+    const { data } = await worker.recognize(canvas);
+
+    if (data && data.text) {
+      this.checkDNI(data.text);
+    }
+
+    await worker.terminate();
+  };
+
+  checkDNI(text: string) {
+    const dnis = text.match(/(.*)([0-9]{8}[A-Z]{1})(.*)/);
+
+    if (dnis && dnis.length > 3) {
+      const dni = dnis[2];
+      this.ocrResult = dni;
+    } else {
+      this.ocrResult = 'N/A';
+    }
+
+    /* const ABC = 'TRWAGMYFPDXBNJZSQVHLCKE';
+    ABC.charAt(nifNumbers % 23); */
   }
 }

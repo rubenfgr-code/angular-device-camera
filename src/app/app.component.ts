b384a91e882a10454357c183ae51d40397850dcf
rubenfgr <rubenfgr87@outlook.com>
2021-07-15 08:19:27 +0200

    add video tag and logic media devices on AppComponent to testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'rf-angular-device-camera';
+  // Prefer camera resolution nearest to 1280x720.
+  constraints = { audio: true, video: { width: 1280, height: 720 } };
+
+  constructor() {
+    navigator.mediaDevices
+      .getUserMedia(this.constraints)
+      .then(function (mediaStream) {
+        const video = document.querySelector('video');
+        if (video) {
+          video.srcObject = mediaStream;
+          video.onloadedmetadata = function (e) {
+            video.play();
+          };
+        }
+      })
+      .catch(function (err) {
+        console.log(err.name + ': ' + err.message);
+      }); // always check for errors at the end.
+  }
 }
